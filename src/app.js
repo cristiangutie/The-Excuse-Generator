b44fdc60046a597d33c1ac78a48b2131e55863e9
Cristián Gutiérrez <cristian.gutie@gmail.com>
2021-05-25 21:39:32 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,5 +4,5 @@ window.onload = function lie() {
   let what = ['my homework', 'the keys', 'the car'];
   let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
   
-  document.getElementById("excuse").innerHTML = who[Math.floor(Math.random() * 3 + 1)] +" "+ action[Math.floor(Math.random() * 3 + 1)] +" "+ what[Math.floor(Math.random() * 2 + 1)] +" "+ when[Math.floor(Math.random() * 4 + 1)];
+  document.getElementById("excuse").innerHTML = who[Math.floor(Math.random() * 4)] +" "+ action[Math.floor(Math.random() * 4)] +" "+ what[Math.floor(Math.random() * 3)] +" "+ when[Math.floor(Math.random() * 5)];
 };
